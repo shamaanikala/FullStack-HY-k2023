6157f0d45643b18e0cafcdfa44a8a9b763186ae8
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-01-27 21:56:19 +0200

    Tehtävä 1.1: Content refaktoroitu

--- osa1/kurssitiedot/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/kurssitiedot/src/App.js
@@ -5,6 +5,15 @@ const Header = (props) => {
   )
 }
 
+const Content = (props) => {
+  console.log(props)
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -17,15 +26,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+     
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
