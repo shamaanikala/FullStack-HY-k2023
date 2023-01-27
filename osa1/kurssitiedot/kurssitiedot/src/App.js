dfe90fcdc726ba628d54827ba820d5e6b0a57f6c
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-01-27 22:01:38 +0200

    Tehtävä 1.1: Total refaktoroitu

--- osa1/kurssitiedot/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/kurssitiedot/src/App.js
@@ -14,6 +14,13 @@ const Content = (props) => {
   )
 }
 
+const Total = (props) => {
+  console.log(props)
+  return (
+    <p>Number of exercises {props.exercisesSum}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -29,8 +36,8 @@ const App = () => {
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
-     
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      
+      <Total exercisesSum={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
