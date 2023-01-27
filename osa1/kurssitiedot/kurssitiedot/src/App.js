13c5208a81b68df604bfcfc33b70bf2594423bc3
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-01-27 23:43:44 +0200

    Tehtävä 1.3: Muutokset tehty ja sovellus toimii

--- osa1/kurssitiedot/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/kurssitiedot/src/App.js
@@ -10,7 +10,7 @@ const Part = (props) => {
   return (
     <>
       <p>
-        {props.part} {props.exercises}
+        {props.part.name} {props.part.exercises}
       </p>
     </>
   )
@@ -21,9 +21,9 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <Part part={props.parts[0]} exercises={props.exercises[0]} />
-      <Part part={props.parts[1]} exercises={props.exercises[1]} />
-      <Part part={props.parts[2]} exercises={props.exercises[2]} />
+      <Part part={props.parts[0]}  />
+      <Part part={props.parts[1]}  />
+      <Part part={props.parts[2]}  />
     </div>
   )
 }
@@ -37,20 +37,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises:  7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
       
-      <Content parts={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]} />
+      <Content parts={[part1,part2,part3]} />
 
-      <Total exercisesSum={exercises1 + exercises2 + exercises3} />
+      <Total exercisesSum={part1.exercises + part2.exercises + part3.exercises} />
+      
     </div>
   )
 }
