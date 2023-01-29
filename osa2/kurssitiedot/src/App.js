75cd3e214810cbd11ac71b5e9ddfe980ab90ab4c
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-01-29 12:42:38 +0200

    Teht.2.1: Lisätään id:t

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -43,18 +43,22 @@ const Total = (props) => {
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises:  7
+        exercises:  7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
   ]
 }
