15fe8012448d009f5d2d87aa98423e6e653944e0
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-01-30 22:37:49 +0200

    T.2.8: Lisätään numerot (step3)

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,9 +2,13 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   // TODO 29.1.2023 19.52
   // TODO: Koska nimiä käytetään id:nä, niin
@@ -24,10 +28,12 @@ const App = () => {
       }
     else {
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -36,6 +42,10 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberField = (event) => {
+    console.log('numerosyöte',event.target.value)
+    setNewNumber(event.target.value)
+  }
 
   return (
     <div>
@@ -46,11 +56,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameField} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberField} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{persons.map(p => <p key={p.name}>{p.name}</p>)}</div>
+      <div>{persons.map(p => <p key={p.name}>{p.name} {p.number}</p>)}</div>
     </div>
   );
 }
