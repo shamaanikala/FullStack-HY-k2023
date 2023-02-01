90d074b7b4a2e2d0d03e27b4018ff250aee35cbe
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-01 22:07:42 +0200

    T2.14: Lisätään remove persons.js moduuliin

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -16,4 +16,10 @@ const update = (id, newObject) => {
     return request.then(response => response.data)
 }
 
-export default { getAll, create, update }
+const remove = (id) => {
+    console.log(`Yritetään poistaa yhteistieto ${id}`)
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+
+export default { getAll, create, update, remove }
