6864d5ac341c6601a87378ba4bfa9450eb6bc3ec
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-01 21:40:16 +0200

    T2.12 puhelinluettelo step7 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -78,9 +78,14 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      axios
+        .post('http://localhost:3001/persons',personObject)
+        .then(response => {
+          console.log(response)
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
     }
   }
 
