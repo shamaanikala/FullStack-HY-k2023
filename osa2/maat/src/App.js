446c2a10311e54ad642301dbe28e5a66da4cbc94
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-03 19:14:41 +0200

    T2.18* Lisätään aihio maille

--- osa2/maat/src/App.js
+++ osa2/maat/src/App.js
@@ -2,17 +2,25 @@ import { useState, useEffect } from "react";
 
 const App = () => {
 
+  const defaultCountiresString = 'Search for country information'
   const [query, setQuery] = useState('');
-  const [countries,setCountries] = useState('Search for country information')
+  const [countries,setCountries] = useState(defaultCountiresString)
 
   const handleQueryField = event => {
     console.log('Hakukenttä',event.target.value)
-    setQuery(event.target.value)
+
+    setQuery(event.target.value) 
   }
+
+  // const countriesToShow = 
+  // const handleQuery = value => {}
 
   return (
     <div>
         find countries <input type="text" value={query} onChange={handleQueryField} />
+        <div>
+          {countries}
+        </div>
     </div>
   );
 }
