e066a18c926f020d97f183cade8cc941b41c5f28
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-04 00:11:48 +0200

    T2.18* Maiden listaus ja haku toimii

--- osa2/maat/src/App.js
+++ osa2/maat/src/App.js
@@ -10,10 +10,16 @@ const CountryName = ({selectedCountry}) => {
 }
 
 const CountryList = ({selectedCountries}) => {
+  if (selectedCountries.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  }
+  // https://stackoverflow.com/questions/39549424/how-to-create-unique-keys-for-react-elements
   return (
     <div>
       {selectedCountries.map(
-        c => <CountryName key={c.id} selectedCountry={c} />
+        (c,ind) => <CountryName key={ind} selectedCountry={c} />
       )}
     </div>
   )
@@ -40,32 +46,65 @@ const App = () => {
       })
   },[])
 
-  const selectCountries = (countries,query) => {
-    console.log('selectCountries',countries[1])
-    setSelectedCountries([].concat(
-      countries[1]
-    ))
+  const selectCountries = (countries,queryValue) => {
+    console.log(
+      'valittu',countries.filter(
+        country => country.name.common.includes(queryValue)
+      )
+    )
+    if (queryValue.length === 0) {
+      setSelectedCountries([])
+    }
+    else {
+      const selected = countries.filter(
+        country => country.name.common.includes(queryValue)
+      )
+      console.log('valittuja maita',selected.length)
+      const selection = [].concat(
+        countries.filter(
+          country => country.name.common.includes(queryValue)
+        ))
+      setSelectedCountries(selection)
+      handleInfoMessage(queryValue,selection)
+    }
+    //console.log('valittuja maita ',selectedCountries.length)
+    
   }
 
   const handleQueryField = event => {
     console.log('Hakukenttä',event.target.value)
     setQuery(event.target.value)
     selectCountries(countries,event.target.value)
-    handleInfoMessage(event.target.value,selectedCountries)
+    if (event.target.value.length === 0) {
+      setInfoMessage(defaultInfoString)
+    }
+      
+    //console.log('handleQueryField kutsuu handleInfoMsg',)
+    //handleInfoMessage(event.target.value,selectedCountries)
   }
 
+  // const handleCountryFilter = (queryValue,countries,countriesToShow) => {
+    
+  // }
 
   const handleInfoMessage = (queryValue,countriesToShow) => {
-    let helper = defaultInfoString
-    if (queryValue.length > 0) {
-      const helper = countriesToShow.length > 10
-        ? tooManyInfoString
-        : null
+    let helper = null
+    console.log('aloitetaan handleInfoMsg',queryValue,countriesToShow.length)
+    if (queryValue.length > 0 && countriesToShow.length !== 0) {
+      helper = null
     }
+    else if (queryValue.length > 0 && countriesToShow.length === 0) {
+      console.log('mitään ei löydy')
+      helper = `Search didn't find any matching country names.`
+    }
+    else helper = defaultInfoString
     console.log('helper',helper)
     setInfoMessage(helper)
   }
 
+  const handleCountryListRender = selectedCountries => {
+
+  }
   // const countriesToShow = 
   // const handleQuery = value => {}
 
