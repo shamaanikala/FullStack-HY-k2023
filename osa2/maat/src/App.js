06df842874f7e1dbab0f3f4fb91d74818cbaf3a0
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-03 19:01:28 +0200

    T2.18*: Lisätään tyhjä syötekenttä

--- osa2/maat/src/App.js
+++ osa2/maat/src/App.js
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
+
 const App = () => {
+
+  const [query, setQuery] = useState(null);
+
   return (
     <div>
-      <p>find countries</p>
+      <p>
+        find countries
+        <input type="text" value="lol" onChange={() => null} />
+        </p>
     </div>
   );
 }
