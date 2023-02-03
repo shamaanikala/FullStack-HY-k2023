cd0aa967ec2e72ed91be0a65394ec912471e30c8
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-03 22:32:28 +0200

    T2.18* Maiden data haetaan paikallisesta json

--- osa2/maat/src/App.js
+++ osa2/maat/src/App.js
@@ -1,25 +1,39 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 
-const App = ({dummyCountries}) => {
+const App = () => {
 
   const defaultHelpString = 'Search for country information'
   const tooManyString = 'Too many matches, specify another filter'
   const [query, setQuery] = useState('');
   const [searchHelp, setSearchHelp] = useState(null)
-  const [countries,setCountries] = useState(null)
+  const [countries,setCountries] = useState([])
+  const [selectedCountries,setSelectedCountries] = useState(null)
+
+  // koska vain get, joten ei tehtä vielä erillistä serviceä
+  useEffect(() => {
+    console.log('useEffect')
+    axios
+      .get('http://localhost:3001/countryData')
+      .then(response => {
+        console.log(response)
+        console.log(response.data)
+        setCountries(countries.concat(response.data))
+      })
+  },[])
 
   const handleQueryField = event => {
     console.log('Hakukenttä',event.target.value)
     setQuery(event.target.value)
-    handleSearchHelp(event.target.value)
+    //handleSearchHelp(event.target.value)
   }
 
-  const handleSearchHelp = queryValue => {
-    const helper = queryValue.length === 0
-      ? defaultHelpString
-      : tooManyString
-      setSearchHelp(helper)
-  }
+  // const handleSearchHelp = queryValue => {
+  //   const helper = queryValue.length === 0
+  //     ? defaultHelpString
+  //     : tooManyString
+  //     setSearchHelp(helper)
+  // }
 
   // const countriesToShow = 
   // const handleQuery = value => {}
@@ -31,7 +45,7 @@ const App = ({dummyCountries}) => {
           {searchHelp}
         </div>
         <div>
-          {countries}
+          {selectedCountries}
         </div>
     </div>
   );
